5222415bf0d745dff34a2e73f2dca0a11532cac6
carlo-ddm <carloduminuco90@gmail.com>
2022-05-04 15:58:39 +0200

    ciclo for degli items

--- script.js
+++ script.js
@@ -15,3 +15,10 @@ Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando s
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se `done` era uguale a `false`, impostare `true` e viceversa)
 */
+
+const app = new Vue ({
+  el: '#app',
+  data: {
+    todos: []
+  }
+})
